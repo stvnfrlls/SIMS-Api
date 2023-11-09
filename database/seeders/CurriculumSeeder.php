19278b430ad3c5b061ece28c6ec04da20a61b602
Steven Frilles <stvnfrlls@gmail.com>
2023-11-09 21:25:19 +0800

    feat: - created CurriculumController - added fillable columns and softDelete traits to model file - added columns to curriculum table - added curriculum data to seeder file - added seeder files to database seeder

---
+++ database/seeders/CurriculumSeeder.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Curriculum;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurriculumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Curriculum::insert([
+            [
+                "subjectCode" => "CBA",
+                "subjectName" => "Character Building Activities",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "3",
+            ],
+            [
+                "subjectCode" => "MT",
+                "subjectName" => "Mother Tongue",
+                "minYearLevel" => "1",
+                "maxYearLevel" => "3",
+            ],
+            [
+                "subjectCode" => "EP",
+                "subjectName" => "Edukasyon sa Pagpapakatao",
+                "minYearLevel" => "1",
+                "maxYearLevel" => "3",
+            ],
+            [
+                "subjectCode" => "FIL",
+                "subjectName" => "Filipino",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "ENG",
+                "subjectName" => "English",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "MTH",
+                "subjectName" => "Math",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "SCI",
+                "subjectName" => "Science",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "AP",
+                "subjectName" => "Araling Panlipunan",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "PE",
+                "subjectName" => "MAPEH",
+                "minYearLevel" => "0",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "EPP",
+                "subjectName" => "Edukasyong Pantahanan at Pangkabuhayan (EPP)",
+                "minYearLevel" => "4",
+                "maxYearLevel" => "6",
+            ],
+            [
+                "subjectCode" => "ESP",
+                "subjectName" => "Edukasyon sa Pagpapakatao (EsP)",
+                "minYearLevel" => "1",
+                "maxYearLevel" => "3",
+            ],
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         CityMunicipality::factory()->count(30)->create();
+
+        $this->call(GradeLevelSeeder::class);
+        $this->call(CurriculumSeeder::class);
     }
 }
