31933de7942b45551bf54d57a896feef28e48e71
Steven Frilles <stvnfrlls@gmail.com>
2023-11-09 21:26:43 +0800

    style: - removed some lines

--- database/seeders/CurriculumSeeder.php
+++ database/seeders/CurriculumSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Curriculum;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CurriculumSeeder extends Seeder
