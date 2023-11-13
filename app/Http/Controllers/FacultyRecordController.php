edb408500aae8166272b609457125ff00177f8e5
Steven Frilles <stvnfrlls@gmail.com>
2023-11-14 01:39:47 +0800

    update FacultyRecordController.php - add fetch for related data from tables - add output formatting

--- app/Http/Controllers/FacultyRecordController.php
+++ app/Http/Controllers/FacultyRecordController.php
@@ -7,46 +7,141 @@ use Illuminate\Http\Request;
 
 class FacultyRecordController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function getAllFaculty()
     {
         $faculties = FacultyRecord::all();
-        return response()->json($faculties);
+
+        $faculties->load("user");
+        $faculties->load("advisoryClasses");
+        $faculties->load("facultySchedule");
+
+        $faculties->each(function ($faculty) {
+            $faculty->advisoryClasses->each(function ($advisoryClass) {
+                $advisoryClass->load('gradeLevel');
+            });
+        });
+
+        $faculties->each(function ($faculty) {
+            $faculty->facultySchedule->each(function ($gradeLevel) {
+                $gradeLevel->load('gradeLevel');
+            });
+        });
+
+        $faculties->each(function ($faculty) {
+            $faculty->facultySchedule->each(function ($subject) {
+                $subject->load('curriculum');
+            });
+        });
+
+        $modifiedData = $faculties->map(function ($facultyRecord) {
+            return $this->transformFacultyRecord($facultyRecord);
+        });
+
+        return response()->json($modifiedData);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function storeFaculty(Request $request)
     {
         $faculty = FacultyRecord::create($request->all());
         return response()->json($faculty);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function getFaculty(FacultyRecord $facultyRecord)
     {
-        return response()->json($facultyRecord);
+        $facultyRecord->load("user");
+        $facultyRecord->load("advisoryClasses");
+        $facultyRecord->load("facultySchedule");
+
+        $facultyRecordCollection = collect([$facultyRecord]);
+
+        $facultyRecordCollection->each(function ($faculty) {
+            $faculty->advisoryClasses->each(function ($advisoryClass) {
+                $advisoryClass->load('gradeLevel');
+            });
+        });
+
+        $facultyRecordCollection->each(function ($faculty) {
+            $faculty->facultySchedule->each(function ($gradeLevel) {
+                $gradeLevel->load('gradeLevel');
+            });
+        });
+
+        $facultyRecordCollection->each(function ($faculty) {
+            $faculty->facultySchedule->each(function ($subject) {
+                $subject->load('curriculum');
+            });
+        });
+
+        $modifiedData = $facultyRecordCollection->map(function ($facultyRecordItem) {
+            return $this->transformFacultyRecord($facultyRecordItem);
+        });
+
+        return response()->json($modifiedData);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function updateFaculty(Request $request, FacultyRecord $facultyRecord)
     {
         $facultyRecord->update($request->all());
         return response()->json($facultyRecord);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroyFaculty(FacultyRecord $facultyRecord)
     {
         $facultyRecord->delete();
+    }
+
+    public function transformFacultyRecord($data)
+    {
+        $data = $data->toArray();
+
+        $facultyRecordKeys = ['id', 'userId', 'gender', 'age', 'birthday', 'deleted_at', 'created_at', 'updated_at'];
+        $userKeys = ['id', 'userId', 'email_verified_at', 'deleted_at', 'created_at', 'updated_at'];
+
+        $modifiedData = array_diff_key($data, array_flip([...$facultyRecordKeys, 'user', 'advisory_classes']));
+        $modifiedData['user'] = array_diff_key($data['user'], array_flip($userKeys));
+
+        $modifiedData += [
+            'userId' => $modifiedData['user']['user_id'],
+            'emailAddress' => $modifiedData['user']['email'],
+            'fullName' => "{$modifiedData['lastName']} {$modifiedData['suffix']}. {$modifiedData['firstName']} {$modifiedData['middleName']}",
+        ];
+        unset(
+            $modifiedData['lastName'],
+            $modifiedData['firstName'],
+            $modifiedData['middleName'],
+            $modifiedData['suffix'],
+            $modifiedData['user']['user_id'],
+            $modifiedData['user']['email'],
+            $modifiedData['user']
+            );
+
+        if (!empty($data['advisory_classes'][0]['grade_level'])) {
+            $gradeLevelData = $data['advisory_classes'][0]['grade_level'];
+            $modifiedData['advisoryClass'] = "{$gradeLevelData['gradeLabel']} - {$gradeLevelData['sectionName']}" ?? null;
+        }
+
+        $facultyScheduleKeys = ['id', 'academicYear', 'subjectId', 'gradeId', 'facultyId', 'deleted_at', 'created_at', 'updated_at'];
+
+        $modifiedData['faculty_schedule'] = array_map(fn($schedule) => array_diff_key($schedule, array_flip($facultyScheduleKeys)), $data['faculty_schedule'] ?? []);
+
+        foreach ($modifiedData['faculty_schedule'] as &$schedule) {
+            if (isset($schedule['grade_level'])) {
+                $gradeLevelData = $schedule['grade_level'];
+                $schedule['gradeSection'] = "{$gradeLevelData['gradeLabel']} - {$gradeLevelData['sectionName']}" ?? null;
+                unset($schedule['grade_level']);
+            }
+        }
+
+        foreach ($modifiedData['faculty_schedule'] as &$subject) {
+            if (isset($subject['curriculum'])) {
+                $subjectNameData = $subject['curriculum'];
+                $subject['subjectName'] = "{$subjectNameData['subjectName']}" ?? null;
+                unset($subject['curriculum']);
+            }
+        }
+
+        return $modifiedData;
     }
 }
