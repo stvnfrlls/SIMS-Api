0da2a965bb13a1ed3251a25654d3fa87f2a5a627
Steven Frilles <stvnfrlls@gmail.com>
2023-11-19 00:51:49 +0800

    update GradeLevelController.php - minor code cleanup

--- app/Http/Controllers/GradeLevelController.php
+++ app/Http/Controllers/GradeLevelController.php
@@ -2,52 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GradeLevelRequest;
 use App\Models\GradeLevel;
-use Illuminate\Http\Request;
 
 class GradeLevelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $gradeLevels = GradeLevel::all();
         return response()->json($gradeLevels);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(GradeLevelRequest $request)
     {
         $gradeLevel = GradeLevel::create($request->all());
         return response()->json($gradeLevel);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(GradeLevel $gradeLevel)
     {
         $gradeLevel = GradeLevel::find($gradeLevel->id);
         return response()->json($gradeLevel);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, GradeLevel $gradeLevel)
+    public function update(GradeLevelRequest $request, GradeLevel $gradeLevel)
     {
+        $gradeLevel = GradeLevel::find($gradeLevel->id);
         $gradeLevel->update($request->all());
         return response()->json($gradeLevel);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(GradeLevel $gradeLevel)
     {
+        $gradeLevel = GradeLevel::find($gradeLevel->id);
         $gradeLevel->delete();
     }
 }
